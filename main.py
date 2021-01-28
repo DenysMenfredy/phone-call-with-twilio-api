b5593b82465668d178abe6eed7a92e0a51b2b085
Denys Menfredy <denys.menfredy7@gmail.com>
2021-01-27 23:26:11 -0300

    added requirements.txt

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from twilio.rest import Client
 
 load_dotenv()
 
-account_sid = os.getenv('TWILIO_ACCOUNT_SID')
-auth_token = os.getenv('TWILIO_AUTH_TOKEN')
+account_sid = os.getenv('TWILIO_ACCOUNT_SID') # put your twilio account_sid here
+auth_token = os.getenv('TWILIO_AUTH_TOKEN') # put your twilio auth_token here
 
 client = Client(account_sid, auth_token)
 
@@ -14,8 +14,8 @@ response_url = 'https://handler.twilio.com/twiml/EHa29a0f5d1b5526a0e2fad5d46dec3
 
 call = client.calls.create(
     url=response_url,
-    to='+5591993213967',
-    from_='+12312599889'
+    to='', # put your number registered in twilio here
+    from_='' # put the number you wanna call here
 )
 
 print(call.sid)
